5c3bd57547fd5e43a83e0650a187e89acbd3576f
Jose Maria <jruanogalvez@gmail.com>
2018-09-17 09:13:59 +0200

    Published to NPM, added minified files and modified names

--- qAnimate.js
+++
@@ -1,5 +0,0 @@
-(function ($) {
-    import('./qAnimate_effects');  // MISCELANEOUS EFFECTS
-    import('./qAnimate_menu');     // MENU LOGIC
-    import('./qAnimate_slider');   // SLIDER LOGIC
-}(jQuery));
---
+++ qanimate.js
@@ -0,0 +1,5 @@
+(function ($) {
+    import('./qanimate_effects');  // MISCELANEOUS EFFECTS
+    import('./qanimate_menu');     // MENU LOGIC
+    import('./qanimate_slider');   // SLIDER LOGIC
+}(jQuery));
